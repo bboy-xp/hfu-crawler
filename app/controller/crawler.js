9519705a038ead8c3b75471d34536ef4b4614eda
unknown <1169470657@qq.com>
2018-12-18 18:46:00 +0800

    add getCourse getScore

--- app/controller/crawler.js
+++ app/controller/crawler.js
@@ -6,20 +6,40 @@ const qs = require('qs');
 const cheerio = require('cheerio');
 
 const login = require('../handlers/login');
+const getScore = require('../handlers/getScore');
+const getCourse = require('../handlers/getCourse');
 
 class CrawlerController extends Controller {
   async login() {
     const ctx = this.ctx;
     const axios = require('axios');
-    const params = {
+    const loginParams = {
       loginData: {
         username: '16105160119',
-        password: 'hyn534629'
+        password: 'hyn534629.'
       },
 
     }
-    await login(params);
-    
+    await login(loginParams);
+    // const getScoreParams = {
+    //   getScoreData: {
+    //     username: '16105160119',
+    //     password: 'hyn534629.',
+    //     xn: '2016',
+    //     xq: '0'
+    //   }
+    // }
+    // await getScore(getScoreParams);
+    const getCourseParams = {
+      getCourseData: {
+        username: '16105160119',
+        password: 'hyn534629.',
+        xn: '2018',
+        xq: '0'
+      }
+    }
+    await getCourse(getCourseParams);
+
     ctx.body = 'ok';
   }
 }
---
+++ app/handlers/getCourse.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+module.exports = async (params, ctx) => {
+  const { cookie, getCourseData } = params;
+  const { username, password, xn, xq } = getCourseData;
+
+  const getCourseRes = await axios({
+    url: 'http://weixin.hrbfu.edu.cn/WeiXinJR/mobile-app/mobileNewService!find_schedule_List.action',
+    method: 'POST',
+    data: `username=${username}&password=${password}&xn=${xn}&xq=${xq}`,
+    headers: {
+      'Accept': 'application/json, text/javascript, */*; q=0.01',
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'X-Requested-With': 'XMLHttpRequest',
+      'Host': 'weixin.hrbfu.edu.cn',
+      'Origin': 'http://weixin.hrbfu.edu.cn',
+      'Referer': 'http://weixin.hrbfu.edu.cn/WeiXinJR/cjcx/index.html',
+      'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16B92 MicroMessenger/6.7.4(0x1607042c) NetType/4G Language/zh_CN'
+    },
+  });
+
+  console.log(getCourseRes.data.data);
+};
